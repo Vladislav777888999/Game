f03b07cc77f0d0f10b73e8ba3913ccf792ff5aa0
Vladislav777888999 <45918245+Vladislav777888999@users.noreply.github.com>
2019-03-15 23:34:39 +0300

    Add files via upload

---
+++ GAME/maps/G1qfeNb2YgQ.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="G1qfeNb2YgQ" tilewidth="64" tileheight="64" spacing="10" tilecount="70" columns="10">
+ <image source="../../../../G1qfeNb2YgQ.jpg" trans="ff00ff" width="750" height="509"/>
+</tileset>
