eddb0e5b9ac90a2f5569d1920c59c9618ea91239
Vladislav777888999 <45918245+Vladislav777888999@users.noreply.github.com>
2019-03-20 00:30:18 +0300

    Add files via upload

---
+++ Hitman/maps/i8tprH_3b4M.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="i8tprH_3b4M" tilewidth="64" tileheight="64" spacing="10" tilecount="30" columns="10">
+ <image source="../../../../i8tprH_3b4M.jpg" trans="ff00ff" width="750" height="269"/>
+</tileset>
