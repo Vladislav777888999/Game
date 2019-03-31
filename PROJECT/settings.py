67eeabb35458cb6f48647fd66cf5c8ce376dbd20
Vladislav777888999 <45918245+Vladislav777888999@users.noreply.github.com>
2019-03-31 23:38:31 +0300

    Update settings.py

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -1,7 +1,6 @@
 import pygame as pg
 vec = pg.math.Vector2
 
-# define some colors (R, G, B)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 DARKGREY = (40, 40, 40)
@@ -11,7 +10,6 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 BROWN = (106, 55, 5)
 
-# game settings
 WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
 HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
 FPS = 60
@@ -24,7 +22,6 @@ GRIDHEIGHT = HEIGHT / TILESIZE
 
 WALL_IMG = 'tileGreen_39.png'
 
-# Player settings
 PLAYER_HEALTH = 100
 PLAYER_SPEED = 280
 PLAYER_ROT_SPEED = 200
@@ -32,7 +29,6 @@ PLAYER_IMG = 'manBlue_gun.png'
 PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
 BARREL_OFFSET = vec(30, 10)
 
-# Gun settings
 BULLET_IMG = 'bullet.png'
 BULLET_SPEED = 500
 BULLET_LIFETIME = 1000
@@ -41,7 +37,6 @@ KICKBACK = 200
 GUN_SPREAD = 5
 BULLET_DAMAGE = 10
 
-# Mob settings
 MOB_IMG = 'zombie1_hold.png'
 MOB_SPEED = 150
 MOB_HIT_RECT = pg.Rect(0, 0, 30, 30)
