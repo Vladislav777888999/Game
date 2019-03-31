c324793fa4a3021ef02bfacf4a846b4373224849
AndreyNuridzhanov <45948290+AndreyNuridzhanov@users.noreply.github.com>
2019-03-31 23:47:31 +0300

    Update settings.py

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -3,8 +3,6 @@ vec = pg.math.Vector2
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-DARKGREY = (40, 40, 40)
-LIGHTGREY = (100, 100, 100)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
@@ -13,7 +11,7 @@ BROWN = (106, 55, 5)
 WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
 HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
 FPS = 60
-TITLE = "Tilemap Demo"
+TITLE = "HITMAN"
 BGCOLOR = BROWN
 
 TILESIZE = 64
